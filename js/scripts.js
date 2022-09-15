47ff04cec94a864b21c677c9ebb849290f2909e7
zhibusiness <zhibusiness@users.noreply.github.com>
2022-09-15 17:05:52 +0800

    Second commit -majel
    Commit changes.

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,39 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+//added by majel 09/15/2022 -a back to top button.
+// Get the button
+let mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+$(document).ready(function(){
+    $('body').one('inview',function(event,isInView) {
+      if (isInView && !$(this).hasClass("caninview")) {
+        $(this).addClass("caninview");
+        $(this).find('.btn-social').addClass("notinview");
+      }
+    });
+    $('.btn-social').one('inview', function(event, isInView) {
+      if (isInView && !$(this).hasClass("inview")) {
+        $(this).removeClass("notinview").addClass("inview");
+      }
+    });
+  });
+  
